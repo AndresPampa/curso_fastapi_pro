60ca8d814efea7e4bda6590753e786808d44a89a
Andres Abdala Alvarez <andrespampa@gmail.com>
2025-10-08 22:43:12 -0300

    Terminando video 5

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 
 app = FastAPI()
@@ -6,6 +7,25 @@ app = FastAPI()
 # app.title = "Mi primera app con FastAPI"
 # app.version =  "2.0.0"
 
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    }
+]
+
+
+
 @app.get('/', tags=['Home'])
 def home() -> str:
     return "hello World!"
+
+@app.get('/movies', tags=['Home'])
+def home() -> HTMLResponse:
+    # return {"hello": "world"}
+    # return HTMLResponse('<h1>Hello World! MADAFAKA!</h1>')
+    return movies
